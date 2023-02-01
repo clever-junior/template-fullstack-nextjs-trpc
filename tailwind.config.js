56ccdf6df0ba04758781399b0d37599feafa2f0b
clever-junior <tk_clever.junior@outlook.com>
2023-02-01 14:46:21 -0400

    feat: add _document file to pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,15 @@ module.exports = {
   mode: 'jit',
   content: [
     './node_modules/flowbite-react/**/*.js',
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
-}
+  plugins: [require('flowbite/plugin')],
+};
